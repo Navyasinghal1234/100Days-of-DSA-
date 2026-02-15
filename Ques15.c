94143948a683be701f312241e4ba2f7c60852c9b
Navyasinghal1234 <write24navya@gmail.com>
2026-02-15 12:56:29 +0530

    Ques15.c

---
+++ Ques15.c
@@ -0,0 +1,22 @@
+// Given a matrix, calculate the sum of its primary diagonal elements. The primary diagonal consists of elements where row index equals column index.
+#include <stdio.h>
+int main() {
+    int m, n;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+    int sum = 0;
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    for(int i = 0; i < m && i < n; i++) {
+        sum += matrix[i][i];
+    }
+    printf("Sum of primary diagonal elements: %d\n", sum);
+
+    return 0;
+}

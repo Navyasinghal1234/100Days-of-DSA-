92ebe2ec6f4d94d04ec37075c5d5284159a80729
Navyasinghal1234 <write24navya@gmail.com>
2026-02-21 11:59:02 +0530

    Ques21.c

---
+++ Ques21.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+void traverseList(struct Node* head) {
+    struct Node* temp = head;
+
+    printf("Linked List: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, value;
+
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        return 0;
+    }
+
+    struct Node* head = NULL;
+    struct Node* temp = NULL;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        struct Node* newNode = createNode(value);
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    traverseList(head);
+
+    return 0;
+}
